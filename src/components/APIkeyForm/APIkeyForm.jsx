befbd04394c5cad8ff9778c56e5dd007b895ce88
nvp85 <nvp@zhukov.al>
2025-05-23 01:52:54 -0500

    create the functionality for saving an api key in the local storage

--- src/components/APIkeyForm/APIkeyForm.jsx
+++ src/components/APIkeyForm/APIkeyForm.jsx
@@ -1,10 +1,37 @@
-
+import { useState, useEffect } from "react";
 
 export default function APIkeyForm() {
     // takes users api key and saves it into the local storage
+    const [apiKey, setApiKey] = useState(null);
+    const [ isStored, setIsStored ] = useState(false);
+
+    useEffect(() => {
+        const storedKey = localStorage.getItem("apiKey");
+        if (storedKey) {
+            setApiKey(storedKey);
+            setIsStored(true);
+        }
+    }, [])
+    
+    function setKey() {
+        localStorage.setItem("apiKey", apiKey);
+        setIsStored(true);
+    }
+
     return (
-        <div>
-            <p>API key form goes here</p>
-        </div>
+        <>
+            <h1>Set your API key</h1>
+            {isStored
+            ? <p>API key is set.</p>
+            : <div>
+                <input type="password" 
+                    name="apiKey"
+                    value={apiKey}
+                    onChange={(e) => setApiKey(e.target.value)}
+                />
+                <button onClick={setKey}>Set</button>
+            </div>
+            }
+        </>
     )
 }

f1efc68019aadfa10701eb89e0bee55ddd5d1f37
nvp85 <nvp@zhukov.al>
2025-05-23 23:38:17 -0500

    create logic of the SignIn component, and html structure of the SignUp component

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,8 +1,73 @@
+import users from "../../assets/users.json";
+import { Link, useNavigate } from 'react-router';
+import { useState, useEffect } from "react";
+import { LuEye, LuEyeOff } from "react-icons/lu";
+import './SignIn.css';
+
 export default function SignIn() {
+    const [formData, setFormData] = useState({
+        email: "demoUser@example.com", 
+        password: "qwerty123"
+    });
+    const [showPassword, setShowPassword] = useState(false);
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    // if there is an info in the local storage then user is logged in
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem("user"));
+        if (storedUser) {
+            navigate("/profile");
+        }
+    }, []);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        // check if there is a matching user in the json
+        const storedUser = users.find(user => user.email === formData.email);
+        if (!storedUser || storedUser.password !== formData.password) {
+            setError("Error: invalid credentials.");
+            return;
+        }
+        localStorage.setItem("user", JSON.stringify(storedUser));
+        navigate("/");
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormData(prev => ({...prev, [name]: value}));
+    }
+
+    function togglePassword(e) {
+        e.preventDefault();
+        setShowPassword(prev => !prev);
+    }
 
     return (
-        <div>
-            <p>Sign in form goes here</p>
+        <div className="form-container">
+            <h1>Sign In</h1>
+            <p>Don't have an accout? Register <Link to="/register">here</Link>.</p>
+            {error ? <p>{error}</p> : "" }
+            <form id="login-form" className="form">
+                <input 
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    placeholder="Email"
+                    onChange={handleChange}
+                    required />
+                <input 
+                    type={showPassword ? "text" : "password"} 
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required 
+                    placeholder="Password"/>
+                <button type="button" onClick={togglePassword}>
+                    {showPassword ? <LuEye /> : <LuEyeOff /> }
+                </button>
+                <button type="submit" onClick={handleSubmit}>submit</button>
+            </form>
         </div>
     )
 }
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,8 +1,25 @@
+import { Link } from 'react-router';
+
+
 export default function SignUp() {
 
+    // validate the form
+    // when a user is created - just save it into the local storage
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <p>Sign up form goes here</p>
+        <div className="form-container">
+            <h1>Sign Up</h1>
+            <p>Already have an accout? Sign in <Link to="/login">here</Link>.</p>
+            <form id="register-form" className="form">
+                <input type="name" placeholder="Name (optional)"/>
+                <input type="email" required placeholder="Email"/>
+                <input type="password" required placeholder="Password"/>
+                <input type="password" required placeholder="Repeat password"/>
+                <button onClick={handleSubmit}>submit</button>
+            </form>
         </div>
     )
 }

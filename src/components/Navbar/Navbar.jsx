2ae33d82dc2135e0ff69d74d7488905f7ea434b1
nvp85 <nvp@zhukov.al>
2025-05-25 02:03:16 -0500

    add conditional rendering based on the current user to the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,31 @@
 import { Link } from "react-router";
 import './Navbar.css'
+import { useState, useEffect } from "react";
+import { useUser } from "../../hooks/UserProvider";
 
+// TODO: conditional rendering: if logged in then "logout" button
 export default function Navbar() {
+    const user = useUser();
+    const isAuthenticated = user.storedUser ? true : false;
+
     return (
         <nav>
             <div className="nav-links">
                 <Link to="/">Logo / Home</Link>
             </div>
             <div className="nav-links">
-                <Link to="api-key">API key</Link>
                 <Link to="about">About</Link>
-                <Link to="profile">Profile</Link>
-                <Link to="register">Sign Up</Link>
-                <Link to="login">Sign In</Link>
+                {isAuthenticated
+                    ? (<>
+                        <Link to="api-key">API key</Link>
+                        <Link to="profile">Welcome, {user.storedUser.name}</Link>
+                        <button onClick={user.removeUser}>Logout</button>
+                    </>)
+                    : (<>
+                        <Link to="login">Sign In</Link>
+                        <Link to="register">Sign Up</Link>
+                    </>)
+                }
             </div>
         </nav>
     )

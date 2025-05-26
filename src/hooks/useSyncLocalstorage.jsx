81b104de8889b4bdc0d7454e31f99c3cbb90a1a3
nvp85 <nvp@zhukov.al>
2025-05-25 20:22:04 -0500

    create useSyncLocalstorage hook

---
+++ src/hooks/useSyncLocalstorage.jsx
@@ -0,0 +1,30 @@
+import { useSyncExternalStore } from "react";
+
+
+export default function useSyncLocalstorage(key) {
+    let lastSnapshot;
+    const getSnapshot = () => {
+        const currentValue = localStorage.getItem(key);
+        if (currentValue !== JSON.stringify(lastSnapshot)) {
+            lastSnapshot = JSON.parse(currentValue);
+        }
+        return lastSnapshot;
+    }
+    const subscribe = (callback) => {
+        window.addEventListener('storage', callback);
+        return () => window.removeEventListener('storage', callback);
+    }
+    const storedValue = useSyncExternalStore(subscribe, getSnapshot);
+
+    const setValue = (newValue) => {
+        newValue = JSON.stringify(newValue);
+        localStorage.setItem(key, newValue);
+        window.dispatchEvent(new StorageEvent('storage', {key, newValue}));
+    }
+
+    const removeValue = () => {
+        localStorage.removeItem(key);
+        window.dispatchEvent(new StorageEvent('storage', {key: key, newValue: null}));
+    }
+    return [storedValue, setValue, removeValue];
+}

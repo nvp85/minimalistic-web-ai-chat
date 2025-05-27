06a04c617be3bd466ff5d069d1a4be62e8f45449
nvp85 <nvp@zhukov.al>
2025-05-27 18:29:10 -0500

    add a hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,18 +3,21 @@ import './Navbar.css'
 import { useState, useEffect } from "react";
 import { useUser } from "../../hooks/UserProvider";
 import { IoHomeOutline } from "react-icons/io5";
+import Hamburger from 'hamburger-react';
 
 // TODO: conditional rendering: if logged in then "logout" button
 export default function Navbar() {
     const user = useUser();
     const isAuthenticated = user.storedUser ? true : false;
+    const [ isMenuOpen, setIsMenuOpen ] = useState();
+
 
     return (
         <nav>
             <div className="nav-links">
                 <NavLink to="/"><IoHomeOutline size="2rem"/> Home</NavLink>
             </div>
-            <div className="nav-links">
+            <div className="nav-links" id="desktop-nav">
                 <NavLink to="about">About</NavLink>
                 {isAuthenticated
                     ? (<>
@@ -27,6 +30,24 @@ export default function Navbar() {
                         <NavLink to="register">Sign Up</NavLink>
                     </>)
                 }
+            </div>
+            <div id="mobil-nav">
+                <Hamburger toggled={isMenuOpen} size={20} toggle={setIsMenuOpen} />
+                { isMenuOpen &&
+                <div className="nav-links" id="hamburger-menu">
+                    <NavLink to="about">About</NavLink>
+                    {isAuthenticated
+                        ? (<>
+                            <NavLink to="chats" id="chats-link">Chats</NavLink>
+                            <NavLink to="profile">Profile</NavLink>
+                            <button onClick={user.removeUser} className="btn">Logout</button>
+                        </>)
+                        : (<>
+                            <NavLink to="login">Sign In</NavLink>
+                            <NavLink to="register">Sign Up</NavLink>
+                        </>)
+                    }
+                </div>}
             </div>
         </nav>
     )

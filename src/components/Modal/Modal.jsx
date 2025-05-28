e759ab01764d040a22e94f554f734bc16ae5ccc3
nvp85 <nvp@zhukov.al>
2025-05-28 11:06:05 -0500

    add an optional button text prop

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,11 @@
 import './Modal.css'
 
-export default function Modal({children, onClose}) {
+export default function Modal({children, onClose, btnText="Cancel"}) {
     return (
         <div className="modal-overlay" onClick={onClose}>
           <div className="modal-content position-relative" onClick={e => e.stopPropagation()}>
             {children}
-            <button onClick={onClose} className='btn'>Cancel</button>
+            <button onClick={onClose} className='btn'>{btnText}</button>
           </div>
         </div>
     );

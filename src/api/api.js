c2ff511de14900701cd29ae3145d2faff68a5380
nvp85 <nvp@zhukov.al>
2025-05-28 14:38:24 -0500

    add a coment to api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
 import OpenAI from "openai";
 
+
+// TODO: check if the convo contains less tokens than the models context window size
 export default async function sendMessage(key, messages) {
     const client = new OpenAI({ apiKey: key, dangerouslyAllowBrowser: true });
     const completion = await client.chat.completions.create({

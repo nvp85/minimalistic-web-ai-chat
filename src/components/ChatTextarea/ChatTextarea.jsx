56d6b633f87c70cefe6897ac3d5311729fe64d6e
nvp85 <nvp@zhukov.al>
2025-05-28 14:39:35 -0500

    wrap the textarea into a form

--- src/components/ChatTextarea/ChatTextarea.jsx
+++ src/components/ChatTextarea/ChatTextarea.jsx
@@ -6,20 +6,22 @@ export default function ChatTextarea(props) {
     const handleClick = props.handleClick;
     const [userInput, setUserInput] = useState("");
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        handleClick(userInput);
+        setUserInput("");
+    }
     return (
-        <div className="input-container">
+        <form className="input-container" onSubmit={handleSubmit}>
             <textarea
                 id="chat-textarea"
                 name='userInput'
                 value={userInput}
                 onChange={(e) => setUserInput(e.target.value)}
-                placeholder="Ask anything...">
+                placeholder="Ask anything..."
+                required>
             </textarea>
-            <button onClick={() => {
-                    handleClick(userInput);
-                    setUserInput("");
-                }
-            }>Send</button>
-        </div>
+            <button type="submit">Send</button>
+        </form>
     )
 }

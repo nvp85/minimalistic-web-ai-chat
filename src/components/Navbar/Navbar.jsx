73ddb27577a882fd9fbd37ff9b6f7d3807c8c475
nvp85 <nvp@zhukov.al>
2025-05-27 19:20:39 -0500

    add tab index to the menu div

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router";
 import './Navbar.css'
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useUser } from "../../hooks/UserProvider";
 import { IoHomeOutline } from "react-icons/io5";
 import Hamburger from 'hamburger-react';
@@ -9,13 +9,19 @@ import Hamburger from 'hamburger-react';
 export default function Navbar() {
     const user = useUser();
     const isAuthenticated = user.storedUser ? true : false;
-    const [ isMenuOpen, setIsMenuOpen ] = useState();
+    const [isMenuOpen, setIsMenuOpen] = useState();
+    const menuRef = useRef();
 
+    useEffect(() => {
+        if (isMenuOpen) {
+            menuRef.current.focus();
+        }
+    }, [isMenuOpen]);
 
     return (
         <nav>
             <div className="nav-links">
-                <NavLink to="/"><IoHomeOutline size="2rem"/> Home</NavLink>
+                <NavLink to="/"><IoHomeOutline size="2rem" /> Home</NavLink>
             </div>
             <div className="nav-links" id="desktop-nav">
                 <NavLink to="about">About</NavLink>
@@ -33,21 +39,25 @@ export default function Navbar() {
             </div>
             <div id="mobil-nav">
                 <Hamburger toggled={isMenuOpen} size={20} toggle={setIsMenuOpen} />
-                { isMenuOpen &&
-                <div className="nav-links" id="hamburger-menu">
-                    <NavLink to="about">About</NavLink>
-                    {isAuthenticated
-                        ? (<>
-                            <NavLink to="chats" id="chats-link">Chats</NavLink>
-                            <NavLink to="profile">Profile</NavLink>
-                            <button onClick={user.removeUser} className="btn">Logout</button>
-                        </>)
-                        : (<>
-                            <NavLink to="login">Sign In</NavLink>
-                            <NavLink to="register">Sign Up</NavLink>
-                        </>)
-                    }
-                </div>}
+                {isMenuOpen &&
+                    <div className="nav-links"
+                        id="hamburger-menu"
+                        ref={menuRef}
+                        onBlur={() => setIsMenuOpen(false)}
+                        tabIndex="0">
+                        <NavLink to="about">About</NavLink>
+                        {isAuthenticated
+                            ? (<>
+                                <NavLink to="chats" id="chats-link">Chats</NavLink>
+                                <NavLink to="profile">Profile</NavLink>
+                                <button onClick={user.removeUser} className="btn">Logout</button>
+                            </>)
+                            : (<>
+                                <NavLink to="login">Sign In</NavLink>
+                                <NavLink to="register">Sign Up</NavLink>
+                            </>)
+                        }
+                    </div>}
             </div>
         </nav>
     )

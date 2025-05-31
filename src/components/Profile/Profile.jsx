bc96aa5ea656433dd07502b019e755d7ce4e0e8f
nvp85 <nvp@zhukov.al>
2025-05-31 12:07:41 -0500

    add editing functionality to profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,25 @@
 import './Profile.css';
+import { useState } from 'react';
 import { Link } from 'react-router';
 import { useUser } from '../../hooks/useUser';
+import ProfileTableRow from './ProfileTableRow';
 
 export default function Profile() {
+    const { currentUser, saveUser } = useUser();
+    // TODO: validate input
+    function saveChanges(field, newValue) {
+        saveUser({...currentUser, [field]: newValue});
+    }
 
     return (
         <div id="profile">
             <h1>User profile</h1>
-            <p>Here the user will be able to edit their username and choose a color scheme for their chat.</p>
+            <table>
+                <tbody>
+                    <ProfileTableRow field="name" value={currentUser.name} saveChanges={saveChanges}/>
+                    <ProfileTableRow field= "email" value={currentUser.email}  saveChanges={saveChanges}/>
+                </tbody>
+            </table>
         </div>
     )
 }
---
+++ src/components/Profile/ProfileTableRow.jsx
@@ -0,0 +1,51 @@
+import { useState, useRef, useEffect } from 'react';
+
+export default function ProfileTableRow(props) {
+    const { field, value, saveChanges } = props;
+    const [editing, setEditing] = useState(false);
+    const [newValue, setNewValue] = useState(value);
+    const [error, setError] = useState("");
+    const editImput = useRef();
+    // TODO: validate input
+    // show error
+    function handleSave() {
+        saveChanges(field, newValue);
+        setEditing(false);
+    }
+
+    function handleEdit() {
+        setEditing(true);
+        setNewValue(value);
+    }
+
+    useEffect(() => {
+        if (editing && editImput.current) {
+            editImput.current.focus();
+        }
+    }, [editing])
+
+    return (
+        <tr>
+            <th>{field[0].toUpperCase() + field.slice(1)}:</th>
+            <td>
+                {editing
+                    ? <input
+                        name="newValue"
+                        value={newValue}
+                        onChange={(e) => setNewValue(e.target.value)}
+                        onBlur={() => setEditing(false)}
+                        ref={editImput}
+                    />
+                    : value}
+            </td>
+            <td>
+                {
+                    editing
+                        ? <button onClick={handleSave} onMouseDown={(e) => e.preventDefault()}>Save</button>
+                        : <button onClick={handleEdit}>Edit</button>
+                }
+            </td>
+        </tr>
+
+    )
+}

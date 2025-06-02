a5a364d684b6a41548bccc8115dfb38031b83618
nvp85 <nvp@zhukov.al>
2025-06-01 23:09:48 -0500

    add some content to the about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -5,7 +5,12 @@ export default function AboutPage() {
     return (
         <div id="about">
             <h1>About this app</h1>
-            <p>This basic React app is my capstone project of Launchcode coding bootcamp.</p>
+            <p>I built it as part of our JavaScript web development course to practice React and React Router. 
+            I was also curious to try out the OpenAI API. It’s a hot topic right now, and I wanted to explore
+             how it works in a real app.
+                The main idea was to keep the UI minimal and focus on core functionality, 
+                such as sending and receiving messages in a chat, and basic chat management 
+                like renaming and deleting chats.</p>
         </div>
     )
 }
